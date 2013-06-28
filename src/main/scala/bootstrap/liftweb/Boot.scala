d4ff95a983ff6d3858a4384c8b365e9da09ebc11
Alexey <lanceon-fake-email@example.fake>
2013-06-28 03:04:37 +0300

    logger added

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -2,8 +2,6 @@ package bootstrap.liftweb
 
 import net.liftweb._
 import util._
-import Helpers._
-
 import common._
 import http._
 import sitemap._
@@ -13,7 +11,7 @@ import Loc._
  * A class that's instantiated early and run.  It allows the application
  * to modify lift's environment
  */
-class Boot {
+class Boot extends Loggable {
 
   def boot {
     // where to search snippet
